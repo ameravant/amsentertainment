0075f51aedcaec46227b5b1e7e904cfe98bbdc29
Macpiece <macpiece@Macpieces-MacBook.local>
2010-08-12 15:08:57 -0700

    inquiry mailer updated w/ sengrid uname

--- vendor/plugins/siteninja/siteninja_core/app/models/inquiry_mailer.rb
+++ vendor/plugins/siteninja/siteninja_core/app/models/inquiry_mailer.rb
@@ -13,13 +13,14 @@ class InquiryMailer < ActionMailer::Base
   
   private
   
-  def setup_email(to_email, to_name, from_email, from_name, subject)
+  def setup_email(email, subject)
     cms_config ||= YAML::load_file("#{RAILS_ROOT}/config/cms.yml")
     recipients   "#{to_name.strip} <#{to_email.strip}>"
     from         "#{CMS_CONFIG['website']['name']} <#{CMS_CONFIG['site_settings']['sendgrid_username']}>"
-    headers      'Reply-to' => "#{from_name.strip} <#{from_email.strip}>"
+    headers      'Reply-to' => "<#{CMS_CONFIG['site_settings']['sendgrid_username']}>"
     subject      subject.strip 
     sent_on      Time.now
     content_type 'text/html'
   end
+
 end
