9dada74c452ea7233602c79e080399ee33f95db9
Macpiece <macpiece@Macpieces-MacBook.local>
2010-09-07 16:27:22 -0700

    fixed pricing and entertainment_type

--- vendor/plugins/ams/app/controllers/entertainments_controller.rb
+++ vendor/plugins/ams/app/controllers/entertainments_controller.rb
@@ -7,12 +7,14 @@ class EntertainmentsController < ApplicationController
       # Filter entertainment by tag
       found_entertainments = Entertainment.find_tagged_with(params[:tag])
       add_breadcrumb params[:tag]
+    elsif params[:entertainment_type]
+      found_entertainments = EntertainmentType.find_by_permalink(params[:entertainment_type]).entertainments
+      add_breadcrumb params[:entertainment_type]
     else
       add_breadcrumb "Entertainment"
       found_entertainments = Entertainment.all    
     end
     @page = Page.find_by_permalink("entertainment")
-    @entertainment_types = EntertainmentType.all
     @entertainments = found_entertainments.paginate(:page => params[:page], :per_page => 50)  
   end
 
@@ -22,7 +24,6 @@ class EntertainmentsController < ApplicationController
       @images = @entertainment.images
     end
     @testimonial = Testimonial.find(:all, :conditions => ["quotable_id = ?" , @entertainment.id]).sort_by(&:rand).first #Select a random testimonial
-    @entertainment_types = EntertainmentType.all
     add_breadcrumb "Entertainment", entertainments_path
     add_breadcrumb @entertainment.title
   end
